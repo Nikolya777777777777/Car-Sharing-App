30570f5d1f47f8c58acc6eb905ed48b1e94b5916
mykola <nikolya.cr@gmail.com>
2025-09-09 16:27:13 +0200

    this commit is only to save changes in tests it is full version of userServiceTest

--- src/test/java/com/example/carsharingapp/service/UserServiceTest.java
+++ src/test/java/com/example/carsharingapp/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.carsharingapp.service;
 import com.example.carsharingapp.dto.user.UpdateRoleRequestDto;
 import com.example.carsharingapp.dto.user.UserRegistrationRequestDto;
 import com.example.carsharingapp.dto.user.UserResponseDto;
+import com.example.carsharingapp.exception.EntityNotFoundException;
 import com.example.carsharingapp.exception.RegistrationException;
 import com.example.carsharingapp.mapper.user.UserMapper;
 import com.example.carsharingapp.model.role.Role;
@@ -18,7 +19,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -158,5 +158,189 @@ public class UserServiceTest {
         verifyNoMoreInteractions(userRepository, userMapper, roleRepository);
     }
 
+    @Test
+    @DisplayName("""
+            Update user's role, but with invalid role name
+            """)
+    public void updateUser_WithInvalidRole_ShouldThrowEntityNotFoundException() {
+        Long id = 1L;
+        UpdateRoleRequestDto updateRoleRequestDto = new UpdateRoleRequestDto()
+                .setRole(ROLE_CUSTOMER);
+
+
+        when(roleRepository.findByName(updateRoleRequestDto.getRole())).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> userService.updateRoleForUser(id, updateRoleRequestDto)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("Role named: "
+                + updateRoleRequestDto.getRole() + " was not found");
+        verify(roleRepository).findByName(updateRoleRequestDto.getRole());
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("""
+            Update user's role, but with invalid user's id
+            """)
+    public void updateUser_WithInvalidUser_ShouldThrowEntityNotFoundException() {
+        Long id = 1L;
+
+        UpdateRoleRequestDto updateRoleRequestDto = new UpdateRoleRequestDto()
+                .setRole(ROLE_CUSTOMER);
+
+        Role role = new Role(ROLE_CUSTOMER)
+                .setId(1L);
+
+
+        when(roleRepository.findByName(role.getName())).thenReturn(Optional.of(role));
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> userService.updateRoleForUser(id, updateRoleRequestDto)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("User with id: "
+                + id + " was not found");
+        verify(roleRepository).findByName(updateRoleRequestDto.getRole());
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("""
+            Get all information about user
+            """)
+    public void getAllInformationAboutUser_WithValidId_ReturnUserResponseDto() {
+        Long id = 1L;
+
+        User user = new User()
+                .setId(1L)
+                .setEmail("nikolya.cr@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk")
+                .setDeleted(false)
+                .setRoles(Set.of(new Role(ROLE_CUSTOMER)));
+
+        UserResponseDto expected = new UserResponseDto()
+                .setEmail("nikolya.cr@gmail.com")
+                .setId(1L)
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk");
+
+        when(userRepository.findById(id)).thenReturn(Optional.ofNullable(user));
+        when(userMapper.modelToResponseDto(user)).thenReturn(expected);
+
+        UserResponseDto result = userService.getAllInfo(id);
+
+        assertTrue(EqualsBuilder.reflectionEquals(result, expected));
+        verify(userRepository).findById(id);
+        verify(userMapper).modelToResponseDto(user);
+        verifyNoMoreInteractions(userRepository, userMapper);
+    }
+
+    @Test
+    @DisplayName("""
+            Get all information about user with invalid id
+            """)
+    public void getAllInfoAboutUser_WithInvalidId_ShouldThrowEntityNotFoundException() {
+        Long id = 1L;
+
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> userService.getAllInfo(id)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("User with id: "
+                + id + " was not found");
+        verify(userRepository).findById(id);
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("""
+            Update information about user
+            """)
+    public void updateInformationAboutUser_WithValidRequest_ReturnUserResponseDto() {
+        Long id = 1L;
+
+        User user = new User()
+                .setId(1L)
+                .setEmail("nikolya.cr@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk")
+                .setPassword("12345678")
+                .setDeleted(false)
+                .setRoles(Set.of(new Role(ROLE_CUSTOMER)));
+
+        UserRegistrationRequestDto requestDto = new UserRegistrationRequestDto()
+                .setEmail("orel@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk")
+                .setPassword("12345678")
+                .setRepeatPassword("12345678");
+
+        User expected = new User()
+                .setId(1L)
+                .setEmail("orel@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk")
+                .setPassword("$2a$10$LLugUeKY.RQnC988wXT2zebftBRFtH0s7uucFS3Mq.t1zhbgQUMYi")
+                .setDeleted(false)
+                .setRoles(Set.of(new Role(ROLE_CUSTOMER)));
+
+        UserResponseDto expectedResult = new  UserResponseDto()
+                .setId(1L)
+                .setEmail("orel@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk");
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(userMapper.updateUser(eq(user), eq(requestDto))).thenReturn(expected);
+        when(passwordEncoder.encode(anyString())).thenReturn(expected.getPassword());
+        when(userRepository.save(any(User.class))).thenReturn(expected);
+        when(userMapper.modelToResponseDto(any(User.class))).thenReturn(expectedResult);
+
+        UserResponseDto result = userService.updateInformationAboutUser(id, requestDto);
+
+        assertTrue(EqualsBuilder.reflectionEquals(expectedResult, result));
+        verify(userRepository).findById(id);
+        verify(userMapper).updateUser(user, requestDto);
+        verify(passwordEncoder).encode(anyString());
+        verify(userRepository).save(any(User.class));
+        verify(userMapper).modelToResponseDto(any(User.class));
+        verifyNoMoreInteractions(userRepository, userMapper, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("""
+            Update information about user with invalid id should throw EntityNotFoundException
+            """)
+    public void updateInfoAboutUser_WithInvalidId_ShouldThrowEntityNotFoundException() {
+        Long id = 1L;
+
+        UserRegistrationRequestDto requestDto = new UserRegistrationRequestDto()
+                .setEmail("orel@gmail.com")
+                .setFirstName("Mykola")
+                .setLastName("Kovbasiuk")
+                .setPassword("12345678")
+                .setRepeatPassword("12345678");
+
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(
+                EntityNotFoundException.class,
+                () -> userService.updateInformationAboutUser(id, requestDto)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("User with id: "
+                + id + " was not found");
+        verify(userRepository).findById(id);
+        verifyNoMoreInteractions(userRepository);
+    }
 
 }
