cf91f917ca83c70bd23aeafab986797bf9f97607
mykola <nikolya.cr@gmail.com>
2025-09-11 22:51:37 +0200

    this commit is only to save changes, I started to implement PaymentServiceTest

--- src/main/java/com/example/carsharingapp/dto/payment/PaymentStatusResponseDto.java
+++ src/main/java/com/example/carsharingapp/dto/payment/PaymentStatusResponseDto.java
@@ -1,6 +1,15 @@
 package com.example.carsharingapp.dto.payment;
 
 import com.example.carsharingapp.model.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
-public record PaymentStatusResponseDto(Status status) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain=true)
+public class PaymentStatusResponseDto {
+    private Status status;
 }
