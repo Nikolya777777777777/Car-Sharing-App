1535d358d5010bb967941bc42b0ebf1f88c6d1e2
mykola <nikolya.cr@gmail.com>
2025-09-18 18:39:20 +0200

    this commit is only to save changes, started implementing tests for controller level

--- src/test/java/com/example/carsharingapp/controller/car/CarControllerTest.java
+++ src/test/java/com/example/carsharingapp/controller/car/CarControllerTest.java
@@ -2,9 +2,8 @@ package com.example.carsharingapp.controller.car;
 
 import com.example.carsharingapp.dto.car.CarRequestDto;
 import com.example.carsharingapp.dto.car.CarResponseDto;
-import com.example.carsharingapp.dto.user.UserRegistrationRequestDto;
-import com.example.carsharingapp.dto.user.UserResponseDto;
 import com.example.carsharingapp.model.enums.Type;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,6 +11,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
@@ -20,10 +22,14 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import java.math.BigDecimal;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -42,14 +48,14 @@ public class CarControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles = {"MANAGER"})
+    @WithMockUser(username = "manager", roles = {"MANAGER"})
     @DisplayName("""
             Create a new user
             """)
     @Sql(scripts = {
             "classpath:database/controller/car/truncate/truncate-cars-table.sql",
     }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void createNewUser_WithValidRequestBody_ReturnCarResponseDto() throws Exception {
+    public void createNewCar_WithValidRequestBody_ReturnCarResponseDto() throws Exception {
         CarRequestDto carRequestDto = new CarRequestDto()
                 .setBrand("Audi")
                 .setModel("A5")
@@ -74,5 +80,58 @@ public class CarControllerTest {
 
         CarResponseDto actual = objectMapper.readValue(result.getResponse().getContentAsString(), CarResponseDto.class);
         assertTrue(EqualsBuilder.reflectionEquals(expected, actual));
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"CUSTOMER"})
+    @DisplayName("""
+            Get all cars
+            """)
+    @Sql(scripts = {
+            "classpath:database/controller/car/add/add-two-cars-to-cars-table.sql",
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(scripts = {
+            "classpath:database/controller/car/truncate/truncate-cars-table.sql",
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getAllCars_WithPageable_ReturnPageOfCarResponseDto() throws Exception {
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        CarResponseDto carResponseDto1 = new CarResponseDto()
+                .setId(1L)
+                .setBrand("Audi")
+                .setModel("A5")
+                .setInventory(50)
+                .setType(Type.SEDAN)
+                .setDailyFee(BigDecimal.valueOf(700).setScale(2));
+
+        CarResponseDto carResponseDto2 = new CarResponseDto()
+                .setId(2L)
+                .setBrand("Mercedes")
+                .setModel("G-class")
+                .setInventory(70)
+                .setType(Type.UNIVERSAL)
+                .setDailyFee(BigDecimal.valueOf(800).setScale(2));
+
+        Page<CarResponseDto> expected = new PageImpl<>(List.of(carResponseDto1, carResponseDto2), pageable, 2);
+        MvcResult result = mockMvc.perform(get("/cars")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        JavaType type = objectMapper.getTypeFactory()
+                .constructParametricType(PageResponse.class, CarResponseDto.class);
+
+        PageResponse<CarResponseDto> actual = objectMapper.readValue(
+                result.getResponse().getContentAsString(), type
+        );
+
+        assertNotNull(actual);
+        assertEquals(expected.getTotalElements(), actual.totalElements);
+        assertTrue(EqualsBuilder.reflectionEquals(expected.getContent().get(0), actual.content.get(0)));
+        assertTrue(EqualsBuilder.reflectionEquals(expected.getContent().get(1), actual.content.get(1)));
+    }
+
+    public static class PageResponse<T> {
+        public List<T> content;
+        public long totalElements;
     }
 }
